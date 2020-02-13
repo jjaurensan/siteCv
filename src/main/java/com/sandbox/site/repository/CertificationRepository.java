c0271f1955285504b288cd55ba971f036ac44ff3
jjaurensan <jj.aurensan@gmail.com>
2020-02-13 21:19:06 +0100

    Correction du CertifcationRepository

--- src/main/java/com/sandbox/site/repository/CertificationRepository.java
+++ src/main/java/com/sandbox/site/repository/CertificationRepository.java
@@ -1,5 +1,9 @@
 package com.sandbox.site.repository;
 
-public class CertificationRepository extends CrudRe{
+import org.springframework.data.repository.CrudRepository;
+
+import com.sandbox.site.beans.Certification;
+
+public interface CertificationRepository extends CrudRepository<Certification, Long> {
 
 }
